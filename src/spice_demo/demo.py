ee9451a97b44eb953fd02d37357f3e1c1061a26f
Piotr Szukalski <95053382+itsthere-academy@users.noreply.github.com>
2022-01-02 21:08:29 +0100

    Add license header

--- src/spice_demo/demo.py
+++ src/spice_demo/demo.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Piotr Szukalski
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #%%
 import spiceypy as spice
 import numpy as np
@@ -73,3 +87,7 @@ fig.add_trace(
 )
 
 fig.show()
+
+#%%
+# and this is how you can save the graph to a file
+# fig.write_html("/tmp/graph.html", include_plotlyjs='cdn', include_mathjax='cdn', full_html=False)
